9b2175b735c80c6fedefa7e7cd72cbadc15c59c0
Abhimanyu Sirohi <abhimanyu.sirohi@perkinelmer.com>
2022-02-15 22:18:38 +0530

    Fix jest coverage run

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   coverageReporters: ["cobertura", "html", "text"],
-  coveragePathIgnorePatterns: ["setupTests.ts", "react-app-env.d.ts"],
-  moduleNameMapper: { "\\.(css)$": "identity-obj-proxy" }
+  coveragePathIgnorePatterns: ["setupTests.ts", "react-app-env.d.ts", "global.d.ts"],
+  moduleNameMapper: { "\\.(css)$": "identity-obj-proxy" },
+  setupFilesAfterEnv: ["./src/setupTests.ts"]
 };
